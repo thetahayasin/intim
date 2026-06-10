f6f08fb50ee8789da1f018927dc12f461a15c136
Taha Yasin <causebroz7@gmail.com>
2026-06-10 17:50:12 +0500

    Login: shrink logo to 48px height icon size

--- resources/views/associate/auth/login.blade.php
+++ resources/views/associate/auth/login.blade.php
@@ -6,7 +6,7 @@
 <div class="col-lg-3 col-md-4 col-sm-9 col-11 mx-auto py-5">
     <div class="text-center mb-4">
         <img class="aalogo" src="{{ asset(\App\Models\Setting::get('site_logo', 'assets/img/logo-full.png')) }}"
-             alt="Asif Associates Logo" style="max-width:120px;height:auto;">
+             alt="Asif Associates Logo" style="height:48px;width:auto;">
     </div>
 
     {{-- Tab switcher --}}
